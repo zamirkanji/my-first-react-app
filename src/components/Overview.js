642e2677ced9738c2980a2e8af068c23f8cfadc4
Zamir Kanji <zamirkanji@comcast.net>
2022-05-23 16:01:08 -0700

    working on edit

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 
 
 const Overview = (props) => {
+
+    const [buttonClicked, setButtonClicked] = useState(false);
     const [editTask, setEditTask] = useState(false);
     const [editButtonText, setEditButtonText] = useState('Edit Task');
     const [deleteButtonText, setDeleteButtonText] = useState('Delete Task');
+
     return (
         <ul>
             {props.tasks.map((task) => {
@@ -13,12 +16,12 @@ const Overview = (props) => {
                         <span contentEditable={editTask} id="task-text">
                             {task.task}
                         </span>
-                        {' '}
+                        {/* {' '} */}
                         <div id="edit-delete-btn-container">
                             <button className="list-buttons" type="button" onClick={() => {
-                                // props.tasks.filter(t => t.id === task)
                                 editButtonText === 'Submit' ? setEditButtonText('Edit Task') : setEditButtonText('Submit');
-                                editTask ? setEditTask(false) : setEditTask(true);
+                                editTask ? setEditTask(false) : setEditTask(true);  
+                                // editButtonText === 'Submit' ? props.setTasks(props.tasks.map(t => console.log(t))) : null;
                             }}>
                                 {editButtonText}
                             </button>
